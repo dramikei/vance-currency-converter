16ff64977a171e8c361cc79bb276d41d0ba1f079
Raghav Vashisht <raghav2011rv@gmail.com>
2023-09-19 23:10:08 +0530

    Initial Commit

---
+++ vance-currency-converter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vance-currency-converter
+//
+//  Created by Raghav Vashisht on 19/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vance-currency-converter/vance_currency_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  vance_currency_converterApp.swift
+//  vance-currency-converter
+//
+//  Created by Raghav Vashisht on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct vance_currency_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
